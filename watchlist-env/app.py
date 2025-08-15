3e46f51634eed95083e2d48e09bc8990247e4af5
msj-dou <msj111@163.com>
2025-08-15 14:13:34 +0800

    添加图片

--- watchlist-env/app.py
+++ watchlist-env/app.py
@@ -1,13 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
-@app.route('/')
-def public_page():
-    return 'Welcome to the public page!'
-@app.route('/user/<username>')
-def user_page(username):
-    print(url_for('user_page', username=username))  # 打印URL
-    return f'User: {escape(username)}'
 
+
+
+@app.route('/')
+def index():
+    username = 'msj'
+    games= [
+            {'title':'动作','name':'奥德赛'},
+            {'title':'大世界探索','name':'荒野之息'},
+            {'title':'角色扮演','name':'巫师3'},
+            {'title':'射击','name':'使命召唤'},
+            {'title':'体育竞技','name':'FIFA23'}
+    ]
+    return render_template('index.html', username=username, games=games)
