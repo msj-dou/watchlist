7d0e7ec19bf845c312ee2dbb89a16f59359fafac
msj-dou <msj111@163.com>
2025-08-15 14:12:55 +0800

    添加图片和样式表

--- watchlist-env/app.py
+++ watchlist-env/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify
+from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
+@app.route('/')
+def public_page():
+    return 'Welcome to the public page!'
+@app.route('/user/<username>')
+def user_page(username):
+    print(url_for('user_page', username=username))  # 打印URL
+    return f'User: {escape(username)}'
 
-def hello():
-    return "Hello, World!"
