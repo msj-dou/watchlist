e79dcbad2076b9c72a46566e578b1904b686a060
msj-dou <msj111@163.com>
2025-08-18 08:59:13 +0800

    我也不知道更新了啥

--- watchlist-env/app.py
+++ watchlist-env/app.py
@@ -1,19 +1,34 @@
 from flask import Flask, request, jsonify,render_template
 from markupsafe import escape
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
+
+prefix = 'sqlite:///'
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
 
-@app.route('/')
-def index():
-    username = 'msj'
-    games= [
-            {'title':'动作','name':'奥德赛'},
-            {'title':'大世界探索','name':'荒野之息'},
-            {'title':'角色扮演','name':'巫师3'},
-            {'title':'射击','name':'使命召唤'},
-            {'title':'体育竞技','name':'FIFA23'}
-    ]
-    return render_template('index.html', username=username, games=games)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gamename = db.Column(db.String(20), nullable=False) 
+    type = db.Column(db.String(20), nullable=False)
+    
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    
+    db.create_all()
+    click.echo('Initialized database.') 
